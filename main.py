cfa5ff6714d6af8e8b84fa9718ff74b3bde97972
Vasiliy-Blokhin <Vasiliyblo97@gmail.com>
2023-08-26 05:34:00 +1000

    result to lesson 7/7

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 
 from random import randint
 
+from grafic_arts.start_game_banner import run_screensaver
+
 
 def attack(char_name: str, char_class: str) -> str:
-
+    """Func do something."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный '
                 f'{5 + randint(3, 5)}')
@@ -19,7 +21,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
-
+    """Func do something."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
 
@@ -30,8 +32,9 @@ def defence(char_name: str, char_class: str) -> str:
         return (f'{char_name} блокировал {10 + randint(2, 5)} урона')
     raise Exception(f"Недопустимый класс {char_class}")
 
-def special(char_name: str, char_class: str) -> str:
 
+def special(char_name: str, char_class: str) -> str:
+    """Func do something."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение «Выносливость '
                 f'{80 + 25}»')
@@ -45,7 +48,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
-
+    """Func do something."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
 
@@ -80,7 +83,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
-
+    """Func do something."""
     approve_choice: str = ''
 
     char_class: str = ''
@@ -111,8 +114,8 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main() -> None:
-
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
 
@@ -126,8 +129,3 @@ def main() -> None:
     char_class: str = choice_char_class()
 
     print(start_training(char_name, char_class))
-    
-
-
-if __name__ == '__main__':
-    main()
